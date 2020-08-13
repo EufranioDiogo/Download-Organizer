3bd6f7552ee4f4b9437757442f493ca27d57a982
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-08-13 01:44:01 +0100

    Actualizando as instruções no install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /opt/download_organizer/
-cp download_organizer.py /opt/download_organizer
+cp download_organizer.py dest_folders.py /opt/download_organizer
 chown $USER /opt/download_organizer/download_organizer.py
 
 chmod +x download_organizer
