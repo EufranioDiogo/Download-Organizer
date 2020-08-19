bffdc73a3517fa0b66e1265710809a8752b172ea
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-08-19 01:08:46 +0100

    Exec for windows created

--- Windows/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir /opt/download_organizer/
-cp download_organizer.py dest_folders.py /opt/download_organizer
-chown $USER /opt/download_organizer/download_organizer.py
-
-chmod +x download_organizer
-cp download_organizer /bin
-chown $USER /bin/download_organizer
