5f43cc7ebd5961a1368a8b2616ca4ecac6f7f19f
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-09-15 19:36:22 +0100

    Mac OS

---
+++ Mac OS/file_extensions.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+music_exten = {
+'mp3', '3gpp', 'wav', 'flac', 'ogg',
+'oga', 'mogg', 'm3u', 'acc', 'm4a',
+'mpa', 'pls', 'aac'}
+
+pictures_exten = {
+'jpg', 'png', 'jpeg', 'gif', 'svg',
+'raw', 'tiff', 'webp'}
+
+documents_exten = {'pdf', 'docx', 'doc', 'txt', 'psd',
+'ai', 'eps', 'ai', 'indd'}
+
+videos_exten = {'mp4', 'm4p', 'm4v', 'webm', 'mpg',
+'mp2', 'mpe', 'mpv','mkv', 'avi',
+'wmv', 'mov', 'flv', 'swf', 'avchd'}
